8d9e325411061e1975b7563fbc1b49c4ff97b093
Severin Fürbringer <severin@protonmail.ch>
2016-01-05 09:24:31 +0100

    Added gcc export options
    The first step to cross platform compatibility

--- include/Engine/EngineExport.h
+++ include/Engine/EngineExport.h
@@ -1,5 +1,21 @@
-#define SEVENGINEAPI_EXPORT __declspec(dllexport)
-#define SEVENGINEAPI_IMPORT __declspec(dllimport)
+#ifdef _MSC_VER
+	#define SEVENGINEAPI_EXPORT __declspec(dllexport)
+	#define SEVENGINEAPI_IMPORT __declspec(dllimport)
+#else
+	#if __GNUC__ >= 4
+		#define SEVENGINEAPI_EXPORT __attribute__ ((visibility ("default")))
+		#define SEVENGINEAPI_IMPORT __attribute__ ((visibility ("default")))
+	#else
+		#define SEVENGINEAPI_EXPORT
+		#define SEVENGINEAPI_IMPORT
+		
+		#ifdef _MSC_VER
+			#pragma NOTE(No export option available on this platform.)
+		#else
+			#warning(No export option available on this platform.)
+		#endif
+	#endif
+#endif
 
 #if defined(SEVENGINE_EXPORTS)
 
